f9911ddede2cf08e450c6fd75cc6c8165a762208
techuse <techuse829@gmail.com>
2021-02-17 16:49:00 +0900

    Add pagination

--- src/models/Task.java
+++ src/models/Task.java
@@ -15,8 +15,12 @@ import javax.persistence.Table;
 @NamedQueries({
     @NamedQuery(
             name = "getAllTasks",
-            query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
-    )
+            query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+            ),
+    @NamedQuery(
+            name = "getTasksCount",
+            query = "SELECT COUNT(t) FROM Task AS t"
+            )
 })
 @Table(name = "tasks")
 public class Task {
